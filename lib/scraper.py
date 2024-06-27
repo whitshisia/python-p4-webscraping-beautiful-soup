d6f8d02947dc221c2ca98e31a80c0f3c74253eeb
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-27 11:28:14 +0300

    flask

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,9 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text,'html.parser')
+courses = doc.select(".heading-60-black.color-black.mb-20")
+ 
+for course in courses:
+    print(course.contents[0].strip()) 
